7121b5f32c387adff4f861dae49fef1ba624035e
cavidanbagiri <cavidanbagiri@gmail.com>
2022-10-25 20:56:15 +0300

    Hide API Key and Use viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,39 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [vue()],
+
+    server: {
+      proxy: {
+        "/api": {
+          // target: "http://localhost:3000",
+          target:process.env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  });
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server:{
-    proxy:{
-      '/api':{
-        // target:import.meta.env.VITE_API_URL,
-        target:'http://localhost:3000',
-        changeOrigin:true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  },
-
-})
+// export default defineConfig({
+//   plugins: [vue()],
+//   server:{
+//     proxy:{
+//       '/api':{
+//         // target:import.meta.env.VITE_API_URL,
+//         target:'http://localhost:3000',
+//         changeOrigin:true,
+//         rewrite: path => path.replace(/^\/api/, '')
+//       }
+//     }
+//   },
+// })
--- client/vue.config.js
+++
@@ -1,9 +0,0 @@
-
-
-// module.exports = {
-
-//     devServer: {
-//         proxy:'http://localhost:3000/'
-//     }
-
-// }
